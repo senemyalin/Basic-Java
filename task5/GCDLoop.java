ee2b24446ee578bffbbc7696da89f489c94c6da1
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-11 17:21:42 +0300

    Add files via upload

---
+++ task5/GCDLoop.java
@@ -0,0 +1,33 @@
+
+public class GCDLoop {
+
+	public static void main(String[] args) {
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
+
+		int divisor=gcd(number1 > number2 ? number1 : number2, 
+						number1 > number2 ? number2 : number1);
+		System.out.println("GCD is " + divisor);
+
+	}
+
+	private static int gcd(int number1, int number2) {
+		int result=0;
+		int remainder;
+		
+		do {
+			System.out.println("Number 1 : "+ number1 + "Number 2 : " + number2);
+			remainder = number1 % number2;
+			number1 = number2;
+			number2 = remainder;
+			result = number1;
+		
+		}while(remainder != 0 );
+	
+	
+		return result;
+		
+		
+	}
+
+}
