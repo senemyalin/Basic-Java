3920a49d3bf189447ff454aab15351dac5f98600
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-12 16:14:42 +0300

    Add files via upload

---
+++ task6/Point.java
@@ -0,0 +1,21 @@
+package lab;
+
+public class Point {
+	int xCoord;
+	int yCoord;
+	
+	
+
+	
+	public Point(int x, int y) {
+		xCoord = x;
+		yCoord = y;
+	}
+
+
+
+
+	public static void main(String[] args) {
+		
+	}
+}
